13f49ae743980918b85a91f6327e2b36766f9911
Matthieu Gicquel <matthieug@bam.tech>
2023-12-12 18:21:03 +0100

    fix: make preventRecentScreenshot: false work on iOS

--- ios/RNASAppLifecyleDelegate.swift
+++ ios/RNASAppLifecyleDelegate.swift
@@ -3,10 +3,17 @@ import UIKit
 
 public class RNASAppLifecycleDelegate: ExpoAppDelegateSubscriber {
     public func applicationDidFinishLaunching(_ application: UIApplication) {
-        application.ignoreSnapshotOnNextApplicationLaunch()
+        if(!isPreventRecentScreenshotsEnabled()) {
+            return
+        }
+            application.ignoreSnapshotOnNextApplicationLaunch()
     }
     
     public func applicationWillResignActive(_ application: UIApplication) {
+        if(!isPreventRecentScreenshotsEnabled()) {
+            return
+        }
+        
         // https://developer.apple.com/documentation/uikit/app_and_environment/scenes/preparing_your_ui_to_run_in_the_background
         
         // TODO: better error handling in case there's an issue with the splashscreen (instead of !)
@@ -17,6 +24,18 @@ public class RNASAppLifecycleDelegate: ExpoAppDelegateSubscriber {
     }
         
     public func applicationDidBecomeActive(_ application: UIApplication) {
+        if(!isPreventRecentScreenshotsEnabled()) {
+            return
+        }
+
         UIApplication.shared.windows.first?.rootViewController?.dismiss(animated: false)
     }
+}
+
+
+func isPreventRecentScreenshotsEnabled() -> Bool {
+    if let value = Bundle.main.object(forInfoDictionaryKey: "RNAS_PREVENT_RECENT_SCREENSHOTS") as? Bool {
+        return value
+    }
+    return false
 }
