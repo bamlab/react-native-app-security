f4de06d62a1571fef0937f0df63289ce2f5ecb1f
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2025-03-10 14:26:48 +0100

    refacto: instanciate the UIWindow only once

--- ios/RNASAppLifecyleDelegate.swift
+++ ios/RNASAppLifecyleDelegate.swift
@@ -2,8 +2,13 @@ import ExpoModulesCore
 import UIKit
 
 public class RNASAppLifecycleDelegate: ExpoAppDelegateSubscriber {
-    private var launchScreenWindow: UIWindow?
-
+    private lazy var launchScreenWindow: UIWindow? = {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let launchScreen = UIStoryboard(name: "SplashScreen", bundle: nil).instantiateInitialViewController()!
+        window.rootViewController = launchScreen
+        window.windowLevel = .alert + 2 // React Native alert uses .alert + 1
+        return window
+    }()
 
     public func applicationDidFinishLaunching(_ application: UIApplication) {
         if(!isPreventRecentScreenshotsEnabled()) {
@@ -20,19 +25,11 @@ public class RNASAppLifecycleDelegate: ExpoAppDelegateSubscriber {
         
         // https://developer.apple.com/documentation/uikit/app_and_environment/scenes/preparing_your_ui_to_run_in_the_background
         
-        let window = UIWindow(frame: UIScreen.main.bounds)
-       
-        let launchScreen = UIStoryboard(name: "SplashScreen", bundle: nil).instantiateInitialViewController()!
-        
-        window.rootViewController = launchScreen
-        window.windowLevel = .alert + 2 // React Native alert uses .alert + 1
-        window.makeKeyAndVisible()
-        self.launchScreenWindow = window
+        launchScreenWindow?.makeKeyAndVisible()
     }
         
     public func applicationDidBecomeActive(_ application: UIApplication) {
         launchScreenWindow?.isHidden = true
-        launchScreenWindow = nil
     }
 }
 
